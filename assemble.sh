14fbaf6128ac75a33505cdee467f06891767d49d
Arda Türkmenoğlu <ardatrkmnoglu@outlook.com>
2026-06-30 23:03:46 +0300

    chore: added assembling & linking script for main files written in assembly

---
+++ assemble.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BUILD_DIR=build
+
+SRC="$1"
+DST="$2"
+
+nasm -f elf64 $SRC -o $BUILD_DIR/$DST.o
+ld $BUILD_DIR/$DST.o -o $BUILD_DIR/$DST
+
+read -n 1 -p "Keep $DST.o? (y/n) " char
+
+case "$char" in
+    [yY]) exit;;
+    [nN]) rm $BUILD_DIR/$DST.o; exit;;
+    *)    exit;;
+esac
